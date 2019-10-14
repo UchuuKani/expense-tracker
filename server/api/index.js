17944cb14fe3f6cb82d18cfafcefa6bda1ee8e54
Alex Yi <probesalesman@gmail.com>
2019-10-13 21:02:24 -0400

    renames express routers to dogs and users from dogsRouter and usersRouter

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
-router.use("/users", require("./usersRouter"));
-router.use("/dogs", require("./dogsRouter"));
+router.use("/users", require("./users"));
+router.use("/dogs", require("./dogs"));
 
 module.exports = router;
 
