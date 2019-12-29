0d8957a1f3c220cca1ff8f2c92517adab3d14578
Alex Yi <probesalesman@gmail.com>
2019-12-29 14:16:39 -0500

    (refactor): changes GET single user route and query to not use row_to_json function on db query

--- server/api/users.js
+++ server/api/users.js
@@ -1,81 +1,72 @@
-const router = require("express").Router();
-const client = require("../db");
-const extendedQueries = require("../queries");
-const {tagParser} = require("../../utils");
+const router = require('express').Router();
+const client = require('../db');
+const extendedQueries = require('../queries');
+const { tagParser } = require('../../utils');
 
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const query = extendedQueries.getAllUsers;
-    const {rows} = await client.query(query);
+    const { rows } = await client.query(query);
 
     res.json(rows);
-  } catch(err) {
+  } catch (err) {
     next(err);
   }
 });
 
 //gets a user and all of their transactions
-router.get("/:id", async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   try {
     const query = extendedQueries.getUserIdTransactions;
-    const {rows} = await client.query(query, [req.params.id]);
+    const { rows } = await client.query(query, [req.params.id]);
 
-    // const {rows} = await client.query(query);
     if (!rows.length) {
-      const new404 = new Error("User not found");
+      const new404 = new Error('User not found');
       new404.status = 404;
       throw new404;
     }
 
-    res.send(rows[0].user);
-  } catch(err) {
+    res.send(rows[0]);
+  } catch (err) {
     next(err);
   }
-})
-
-//gets a specific user's specific transaction
-router.get("/:id/transactions/:transactionId", async (req, res, next) => {
-  try {
-    const query = extendedQueries.getSpecificTransaction;
-    const {rows} = await client.query(query, [req.params.id]);
-
-    res.send(rows)
-  } catch(err) {
-    next(err);
-  }
-})
+});
 
 //posts a new user to the database
-router.post("/", async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   try {
-    const {name, email} = req.body
+    const { name, email } = req.body;
     const query = extendedQueries.postNewUser;
 
-    const {rows} = await client.query(query, [name, email]);
+    const { rows } = await client.query(query, [name, email]);
 
     res.status(201).send(rows[0]);
-  } catch(err) {
+  } catch (err) {
     next(err);
   }
 });
 
 //QUERY NOT COMPLETE - DOES NOT ADD TO TAGS JOIN TABLE
 //posts a new transcation for a specific user, and includes tags
-router.post("/:id", async (req, res, next) => {
+router.post('/:id', async (req, res, next) => {
   try {
-    const {description, amount, tags} = req.body;
+    const { description, amount, tags } = req.body;
     //have to process tags to insert into table, assuming it is a comma separated string
     const processedTags = tagParser(tags);
 
     //post into transactions, tags, and tags_transactions
 
     const queryTransactions = extendedQueries.postNewUserTransaction;
-    const {rows} = await client.query(queryTransactions, [description, amount, req.params.id]);
+    const { rows } = await client.query(queryTransactions, [
+      description,
+      amount,
+      req.params.id,
+    ]);
 
     const queryTags = extendedQueries.postTagsOnTransaction;
 
     res.status(201).send(rows[0]); //rewrite to send back newly created task or redirect to task list for user
-  } catch(err) {
+  } catch (err) {
     next(err);
   }
 });
--- server/queries.js
+++ server/queries.js
@@ -1,22 +1,64 @@
-const getAllUsers = "SELECT users.* FROM users";
+const getAllUsers = 'SELECT users.* FROM users';
 
-const getUserIdTransactions = "SELECT row_to_json(t) AS user FROM \
+//do another subquery within query starting on line 8 to grab all tags associated with a transaction
+//something like ...FROM transactions JOIN (SELECT json_agg(row_to_json(table_alias)))
+
+const getUserIdTransactions =
+  'SELECT t.* FROM \
   (SELECT id, name, email, \
   (SELECT json_agg(row_to_json(transactions)) \
     FROM transactions \
       WHERE user_id = users.id) AS user_transactions \
       FROM users \
         WHERE id = $1\
-  ) t";
+  ) t';
 
-//do I just want to grab tags for specific transactionId, or grab the specific transaction along with all associated tags, and just pass down transaction info down from front end
-const getSpecificTransaction = "SELECT id, description, amount, array_agg(t) AS tags FROM transactions LEFT JOIN tags USING (user_id) WHERE user_id = $1";
+//do I just want to grab tags for specific transactionId, or grab the specific transaction along with all associated tags, and just pass down transaction info down from front end -- my answer is grab the user and all (or some) transactions with transactions joined to tags
+const getSpecificTransaction =
+  'SELECT id, description, amount, array_agg(t) AS tags FROM transactions LEFT JOIN tags USING (user_id) WHERE user_id = $1';
 
-const postNewUser = "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *";
+const postNewUser =
+  'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *';
 
-const postNewUserTransaction = "INSERT INTO transactions (description, amount, user_id) VALUES ($1, $2, $3) RETURNING *";
+const postNewUserTransaction =
+  'INSERT INTO transactions (description, amount, user_id) VALUES ($1, $2, $3) RETURNING *';
 
-const postTagsOnTransaction = "";
+// select users.*, json_agg(f) from (select row_to_json(t) AS transactions from
+// (select * from transactions where user_id = users.id) t) f
+// join users
+// on users.id = transactions.id
+// where users.id = 1;
+
+const postTagsOnTransaction =
+  'SELECT t.* AS user FROM \
+  (SELECT id, name, email, \
+  (SELECT json_agg(row_to_json(transactions)) \
+    FROM transactions \
+      WHERE user_id = users.id) AS user_transactions \
+      FROM users \
+        WHERE id = 1\
+  ) t';
+
+// this query selects a single transaction with id = 2, as well as all the tag_ids associated with that transaction from the tags_transactions table
+
+// -- join transactons to tags_transactions
+// -- join resulting table of "transactions to tags_transactions"
+// -- to tags
+
+const kindOfWorks =
+  'select transactions.*, tags_transactions.* from \
+transactions JOIN tags_transactions \
+on transactions.id = tags_transactions.transaction_id \
+where transactions.id = 2';
+
+const alsoWorksSortOf =
+  'SELECT transactions.*, \
+tags.* \
+from transactions \
+join tags_transactions \
+on transactions.id = tags_transactions.transaction_id \
+join tags on tags.id = tags_transactions.tag_id \
+where transactions.id = 2';
 
 module.exports = {
   getAllUsers,
@@ -24,5 +66,5 @@ module.exports = {
   getSpecificTransaction, //is this needed? I'll try to write it anyway, even if this can just be shown on front-end (for practice)
   postNewUser,
   postNewUserTransaction, //not written
-  postTagsOnTransaction //not written
-}
+  postTagsOnTransaction, //not written
+};
