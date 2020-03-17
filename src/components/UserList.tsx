69f390fb8f6bc3a168c22c91244d05665e726c61
Alex Y <probesalesman@gmail.com>
2020-03-17 17:31:15 -0400

    adds tsx extension to component files and fixes type declaration in Routes.tsx

---
+++ src/components/UserList.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const UserList = props => {
+  const [users, setUsers] = useState({ users: [] });
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get("/api/users");
+      setUsers(data);
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>This is a list of all registered users</h1>
+      <div className="user-list">
+        {users.length &&
+          users.map(user => {
+            return (
+              <Link key={user.id} to={`/users/${user.id}`}>
+                <h3>{user.name}</h3>
+              </Link>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default UserList;
