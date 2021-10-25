a948d3bae3ac12e95f3b471ba4021ebebff81bfe
alex <probesalesman@gmail.com>
2021-10-24 22:44:27 -0400

    adds basic test to run on github actions events

---
+++ src/components/AddTransaction/AddTransaction.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import AddTransaction from "./AddTransaction";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<AddTransaction />, div);
+});
